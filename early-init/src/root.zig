f3547fe619ae0c78af0e63f9dbe9d62d275ee6af
xtex <xtexchooser@duck.com>
2024-02-08 08:47:19 +0800

    early-init: check kernel

--- early-init/src/root.zig
+++ early-init/src/root.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const log = std.log;
 pub const hyplog = @import("hyplog");
 
 pub const std_options = struct {
@@ -13,4 +14,24 @@ pub fn init_logger() void {
         hyplog.openDevConsole() catch null,
         hyplog.openPmsg() catch null,
     } ++ .{null} ** 13;
+}
+
+pub fn check_kernel() void {
+    const utsname = std.os.uname();
+    if (std.mem.indexOf(u8, &utsname.version, "hyperpsi") == null) {
+        log.warn("The kernel seems not to be for HyperPsi", .{});
+    }
+    log.info("{s} {s} {s}", .{ utsname.sysname, utsname.release, utsname.version });
+}
+
+pub fn strict_check_kernel() error{KernelNotSuitable}!void {
+    const utsname = std.os.uname();
+    if (std.mem.indexOf(u8, &utsname.version, "hyperpsi") == null) {
+        log.err("The kernel seems not to be for HyperPsi: {s} {s} {s}", .{
+            utsname.sysname,
+            utsname.release,
+            utsname.version,
+        });
+        return error.KernelNotSuitable;
+    }
 }
