b63bb3de3340df86f94415d0406bba90010c5bac
xtex <xtexchooser@duck.com>
2024-02-10 11:09:31 +0800

    device-build: external: Sync submodule before update

--- device-build/external.mk
+++ device-build/external.mk
@@ -7,7 +7,9 @@ $1-srctree ?= $$($1-gitdir)/$$($1-moddir)
 $1-githead ?= $$($1-gitdir)/.git/modules/$$($1-gitmod)/HEAD
 
 quiet-cmd-submod-$1='SYNC     $1'
-cmd-submod-$1=git -C $$($1-gitdir) submodule update --init --recommend-shallow --recursive --single-branch -- $$($1-moddir); $$($1-postsync)
+cmd-submod-$1=git -C $$($1-gitdir) submodule sync --recursive -- $$($1-moddir); \
+	git -C $$($1-gitdir) submodule update --init --recommend-shallow --recursive --single-branch -- $$($1-moddir); \
+	$$($1-postsync)
 
 $$($1-githead): $$($1-gitdir)/.gitmodules
 	$$(call cmd,submod-$1)
