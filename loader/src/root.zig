75e27f943ab27253012235b5a313f95aefd99d7c
xtex <xtexchooser@duck.com>
2024-02-14 10:30:14 +0800

    loader: Fix compile error

--- loader/src/root.zig
+++ loader/src/root.zig
@@ -5,9 +5,10 @@ const init = @import("hypinit");
 
 pub const BootConfig = init.BootConfig;
 pub const mount = init.mount;
+pub const std_options = init.std_options;
 pub const initLogger = init.initLogger;
 pub const exec = init.exec;
-pub var bootconfig = init.bootconfig;
+pub var bootconfig: BootConfig = undefined;
 
 pub fn prepareLoader() !void {
     try std.os.chdir("/loader");
