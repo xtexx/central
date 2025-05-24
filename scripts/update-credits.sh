a0ecab697a759c15c48899193bff8efce6369639
xtex <xtexchooser@duck.com>
2025-05-24 13:36:58 +0800

    scripts: update-credits: Sort Modrinth links

--- scripts/update-credits.sh
+++ scripts/update-credits.sh
@@ -5,7 +5,7 @@ $PACKWIZ list | sort | uniq | awk '{ print "- " $0 }' | node -e "const fs = requ
 
 git log --pretty='%aN' | sort | uniq | awk '{ print "- " $0 }' | node -e "const fs = require('fs'); fs.writeFileSync('CREDITS.md', fs.readFileSync('CREDITS.md').toString().replace(/<!--BEGIN CONTRIBUTORS LIST-->((.|\\n)*)<\!--END CONTRIBUTORS LIST-->/m, '<!--BEGIN CONTRIBUTORS LIST-->\\n\\n' + fs.readFileSync('/dev/stdin') + '\\n<\!--END CONTRIBUTORS LIST-->'))"
 
-grep '^mod-id = ".*"$' --include='*.pw.toml' -r . --with-filename --only-matching | sed -e 's/:.*//g' | while read -r file
+grep '^mod-id = ".*"$' --include='*.pw.toml' -r . --with-filename --only-matching | sed -e 's/:.*//g' | sort | while read -r file
 do
 name=$(grep '^name = ".*"$' "$file" --no-filename | sed -e 's/name = "//' | sed -e 's/"//')
 modid=$(grep '^mod-id = ".*"$' "$file" --no-filename | sed -e 's/mod-id = "//' | sed -e 's/"//')
