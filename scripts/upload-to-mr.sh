ca8a5bb22e4ef9f9affb9d375af8a2ef9cc7a4c2
xtex <xtexchooser@duck.com>
2025-05-24 13:44:53 +0800

    scripts: upload-to-mr: fix upload payload without changelog

--- scripts/upload-to-mr.sh
+++ scripts/upload-to-mr.sh
@@ -2,16 +2,19 @@
 # shellcheck source=/dev/null
 source .env
 
+set -euo pipefail
+
 version=$(grep '^version = "' pack.toml | sed -e 's/version = "//' | sed -e 's/"//')
 echo "$version"
 
-payload=$(jo -- project_id=HsMwyVxf \
+payload=$(jo -- \
+    project_id=HsMwyVxf \
     file_parts="$(jo -a file)" featured=false loaders="$(jo -a fabric)" \
     version_type=release \
     game_versions="$(jo -a -- -s "$(grep '^minecraft = "' pack.toml | sed -e 's/minecraft = "//' | sed -e 's/"//')")" \
     dependencies="$(jo -a < /dev/null)" \
     -s "version_number=$version" -s "name=$version" \
-    "changelog=@$CHANGELOG")
+    "changelog=@${CHANGELOG:-/dev/null}")
 
 echo "$payload"
 
