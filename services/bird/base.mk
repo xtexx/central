4f034fae04e9d553f577826c84e28c2346de8546
xtex <xtexchooser@duck.com>
2025-11-30 08:31:41 +0800

    BIRD: Passthrough real hostname
    Change-Id: I6970a464db694e1aa4b3c68f102f679157c5b334

--- services/bird/base.mk
+++ services/bird/base.mk
@@ -15,6 +15,7 @@ V_ARGS		+= --mount=type=bind,src=/var/cache/bird,dst=/var/cache/bird
 V_ARGS		+= --mount=type=bind,src=/var/run/bird,dst=/var/run/bird
 V_ARGS		+= --publish-all
 V_ARGS		+= --network=host --ipc=host --cap-add=CAP_NET_ADMIN --privileged
+V_ARGS		+= --hostname="$(HOSTNAME)"
 V_ARGS 		+= codeberg.org/xens/bird:$(strip $(BIRD_VERSION))
 V_ARGS 		+= -R
 $(call end)
