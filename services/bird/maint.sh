0f18e100a9be2b429090ca8b78fb6732b1ef9d2d
xtex <xtexchooser@duck.com>
2026-02-15 17:42:36 +0800

    BIRD: Use BIRD 2 in validation script
    Change-Id: I4171a9425871bb7030a51d29e443d83c8f754d06

--- services/bird/maint.sh
+++ services/bird/maint.sh
@@ -41,7 +41,7 @@ atre::bird::validate() {
 
 	podman run -it --rm --name bird-validate -v "$(pwd)":/validate \
 		--privileged \
-		codeberg.org/xens/bird:latest \
+		codeberg.org/xens/bird:2.18 \
 		-p -c /validate/.bird-valid.conf
 
 	rm -f .bird-valid.conf
