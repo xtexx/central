5ebfe312e9c8e11f6b19505a8069383a4b115ca4
xtex <xtex@astrafall.de>
2026-02-19 09:39:03 +0800

    cotton: Add
    Change-Id: I9e97cba8d4cdc35119eba3de1468a7b14edeeb77

---
+++ hosts/cotton.s.xvnet0.eu.org.mk
@@ -0,0 +1,9 @@
+### tiang::target cotton ssh://cotton.s.xvnet0.eu.org
+
+XVNET_NUM := 3
+
+### tiang::tag cotton bird
+$(call load-state, services/bird)
+
+### tiang::tag cotton caddy
+$(call load-state, services/caddy)
