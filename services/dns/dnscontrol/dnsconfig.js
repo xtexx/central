9797844a210be3bd71113ed095bf76aaf46be4c5
xtex <xtex@astrafall.de>
2026-02-19 11:29:48 +0800

    DNS: Remove xvnetstatus.us.kg
    Change-Id: Ibd5074dec750ced4b0bb1409c58e6735d8f653c6

--- services/dns/dnscontrol/dnsconfig.js
+++ services/dns/dnscontrol/dnsconfig.js
@@ -13,9 +13,6 @@ D_EXTEND("xvnet0.eu.org", DnsProvider(DSP_DESEC, 0));
 require("converted/xvnet.eu.org.js");
 D_EXTEND("xvnet.eu.org", DnsProvider(DSP_GCORE, 0));
 
-require("converted/xvnetstatus.us.kg.js");
-D_EXTEND("xvnetstatus.us.kg", DnsProvider(DSP_DESEC, 0));
-
 // var domains = getConfiguredDomains();
 // for (i = 0; i < domains.length; i++)
 // 	D_EXTEND(domains[i], DnsProvider(DSP_BIND));
